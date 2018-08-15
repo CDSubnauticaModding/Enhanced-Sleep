7d0d4e47f5fbb365a2439bf73e8148083172da55
doriannaujokat <35139623+doriannaujokat@users.noreply.github.com>
2018-08-15 14:02:28 +0200

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Harmony;
+
+namespace Subnautica_Enhanced_Sleep
+{
+    public class Main
+    {
+        public static HarmonyInstance hinstance;
+        
+        public static string fileName;
+
+        public static void Patch()
+        {
+            fileName = "" + DateTime.Now.Day + "-" + DateTime.Now.Month + "-" + DateTime.Now.Year + "--" +
+                       DateTime.Now.Hour + "-" + DateTime.Now.Minute + "-" + DateTime.Now.Second + ".log";
+            try
+            {
+                hinstance = HarmonyInstance.Create("subnauticaenhancedsleep");
+                hinstance.PatchAll(Assembly.GetExecutingAssembly());
+                Log("Patched Successfully");
+            }
+            catch (Exception ex)
+            {
+                Log("[FATAL CRITICAL ERROR] Could not Patch: " + ex.Message);
+                Log("[FATAL CRITICAL ERROR] Stack Trace: " + ex.StackTrace);
+            }
+        }
+
+        public static void Log(string message)
+        {
+            Console.WriteLine("[Enhanced Sleep] <" + DateTime.Now.ToString("HH:mm:ss") + "> " + message);
+            string logDir = Path.GetDirectoryName(Assembly.GetCallingAssembly().Location);
+            File.AppendAllText(logDir + "/" + fileName, "<" + DateTime.Now.ToString("HH:mm:ss") + "> " + message + "\n");
+        }
+    }
+}
---
+++ SleepPatcher.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using Harmony;
+
+namespace Subnautica_Enhanced_Sleep
+{
+    class SleepPatcher
+    {
+        public static float wentToSleep = 0;
+
+        [HarmonyPatch(typeof(Bed))]
+        [HarmonyPatch("EnterInUseMode")]
+        private class EnterPatch
+        {
+            private static bool Prefix(Bed __instance)
+            {
+                wentToSleep = DayNightCycle.main.GetDayNightCycleTime();
+                return true;
+            }
+        }
+
+        [HarmonyPatch(typeof(Bed))]
+        [HarmonyPatch("OnHandClick")]
+        private class HandPatch
+        {
+            private static bool Prefix(Bed __instance, GUIHand hand)
+            {
+                if (hand.player.GetComponent<Survival>().food <= 25 || Player.main.GetComponent<Survival>().water <= 25)
+                {
+                    if (hand.player.GetComponent<Survival>().food <= 25 &&
+                        hand.player.GetComponent<Survival>().water <= 25)
+                    {
+                        ErrorMessage.AddWarning("You can't sleep now because you are too hungry and thirsty! Drink and eat something before you go to bed.");
+                    } else if (hand.player.GetComponent<Survival>().food <= 25 ||
+                               !(hand.player.GetComponent<Survival>().water <= 25))
+                    {
+                        ErrorMessage.AddWarning("You can't sleep now because you are too hungry! Go eat something before you go to bed.");
+                    } else if (!(hand.player.GetComponent<Survival>().food <= 25) ||
+                               hand.player.GetComponent<Survival>().water <= 25)
+                    {
+                        ErrorMessage.AddWarning("You can't sleep now because you are too thirsty! Drink something before you go to bed.");
+                    }
+                    else
+                    {
+                        ErrorMessage.AddWarning("You can't sleep now! Try to drink and eat something and try again!");
+                    }
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        [HarmonyPatch(typeof(Bed))]
+        [HarmonyPatch("ExitInUseMode")]
+        private class ExitPatch
+        {
+            private static void Postfix(Bed __instance, Player player)
+            {
+                player.liveMixin.health = player.liveMixin.maxHealth;
+                player.liveMixin.health = player.liveMixin.maxHealth;
+                Survival sv = player.GetComponent<Survival>();
+                float stoodUp = DayNightCycle.main.GetDayNightCycleTime();
+                float foodBefore = sv.food;
+                float waterBefore = sv.water;
+                float timeSlept = 0;
+                if (wentToSleep > 0.5)
+                {
+                    timeSlept = (((DayNightCycle.main.GetDayNightCycleTime() + 1) - wentToSleep));
+                }
+                else
+                {
+                    timeSlept = (((DayNightCycle.main.GetDayNightCycleTime() + 0) - wentToSleep));
+                }
+
+                double hourTimeSlept = timeSlept / (1d / 24);
+                double minuteTimeSlept = hourTimeSlept * 60;
+
+                double looseFactor = 0.5;
+                double foodLost = minuteTimeSlept / (25.20 * looseFactor);
+                double waterLost = minuteTimeSlept / (18.00 * looseFactor);
+                float foodAfter = foodBefore - (float)foodLost;
+                float waterAfter = waterBefore - (float)waterLost;
+                if (foodAfter > 5 ) {sv.food = foodAfter;} else {sv.food = 5;}
+                if (waterAfter > 5) { sv.water = waterAfter; } else { sv.water = 5; }
+                Main.Log("!!Left Bed:\nWent to Bed Time: " + wentToSleep + "\nWoke up: " + stoodUp + "\nDuration: " + timeSlept + "\nDuration in IGHours: " + hourTimeSlept + "\nDuration in IGMinutes: " + minuteTimeSlept + "\nFood Before: " + sv.food + "\nFood Lost: " + foodLost + "\nFood After: " + sv.food + "\nWater Before: " + waterBefore + "\nWater Lost: " + waterLost + "\nWater After: " + sv.water);
+                
+            }
+        }
+    }
+}
